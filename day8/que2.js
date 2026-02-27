747b41b96f0f139b54af04c0c54e45b7eb40be68
24f2002727 <shivamrock811307@gmail.com>
2026-02-27 18:38:42 +0530

    day8 Q2.js done

---
+++ day8/que2.js
@@ -0,0 +1,15 @@
+// let 1age=5;  variable name con not start with number
+let age1=5;
+let a2ge=10;
+sum=age1+a2ge;
+console.log(`sum is ${sum}`);
+
+let marks=75;
+// let isPass=True; //bool true is in small letters
+// console.log(isPass);
+
+let isPass='true'; //it is string not a bool expression
+console.log(isPass);
+console.log(typeof(isPass));
+
+
