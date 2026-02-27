98cc20c15a2676008ec031b9dd2e6c88ce619c40
24f2002727 <shivamrock811307@gmail.com>
2026-02-27 18:39:05 +0530

    day8 Q3.js done

---
+++ day8/que3.js
@@ -0,0 +1,13 @@
+let s='shivam';
+console.log(s.length)
+
+console.log(s[0]);
+
+console.log(s[s.length-1]);
+
+console.log("shivam"+74);
+
+a="";
+b=" ";
+console.log(a.length);
+console.log(b.length);
